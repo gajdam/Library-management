6692cc81bea17d4b7e27192048912856f4c7ef14
Maciej Gajda <gajdam11@gmail.com>
2023-06-12 14:58:34 +0200

    admin class

---
+++ Admin.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gajda on 12.06.2023.
+//
+
+#include "Admin.h"
---
+++ Admin.h
@@ -0,0 +1,14 @@
+//
+// Created by gajda on 12.06.2023.
+//
+
+#ifndef LIBRARY_MANAGEMENT_ADMIN_H
+#define LIBRARY_MANAGEMENT_ADMIN_H
+
+
+class Admin {
+
+};
+
+
+#endif //LIBRARY_MANAGEMENT_ADMIN_H
